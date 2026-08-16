ef47b3d2ce8459ba1fe9f4d983e397f3c2160a6c
rjfovo <rjf12138@gmail.com>
2026-08-16 12:54:30 +0800

    代码提交

--- translations/en_US.ts
+++ translations/en_US.ts
@@ -4,52 +4,42 @@
 <context>
     <name>AppPage</name>
     <message>
-        <location filename="../qml/AppPage.qml" line="42"/>
         <source>Maintainer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/AppPage.qml" line="54"/>
         <source>Homepage</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/AppPage.qml" line="74"/>
         <source>Installed Size</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/AppPage.qml" line="84"/>
         <source>Description</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/AppPage.qml" line="127"/>
         <source>Version: </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/AppPage.qml" line="128"/>
         <source>Installed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/AppPage.qml" line="129"/>
         <source> (Installed Version: %1) </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/AppPage.qml" line="173"/>
         <source>Reinstall</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/AppPage.qml" line="168"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/AppPage.qml" line="173"/>
         <source>Install</source>
         <translation type="unfinished"></translation>
     </message>
@@ -57,75 +47,57 @@
 <context>
     <name>DebInstaller</name>
     <message>
-        <location filename="../src/debinstaller.cpp" line="257"/>
-        <source>Error: Wrong architecture %1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/debinstaller.cpp" line="274"/>
-        <source>Error: Breaks the existing package %1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/debinstaller.cpp" line="282"/>
         <source>Error: Cannot satisfy dependencies</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/debinstaller.cpp" line="290"/>
-        <source>Requires the installation of %1 additional package.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/debinstaller.cpp" line="292"/>
-        <source>All dependencies are satisfied.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/debinstaller.cpp" line="480"/>
-        <source>Starting</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/debinstaller.cpp" line="486"/>
-        <source>Waiting</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/debinstaller.cpp" line="492"/>
-        <source>Installing</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/debinstaller.cpp" line="498"/>
-        <source>Loading Cache</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/debinstaller.cpp" line="503"/>
-        <source>Downloading Packages</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/debinstaller.cpp" line="516"/>
         <source>Installation successful</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/debinstaller.cpp" line="531"/>
         <source>Installation failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to initialize APT configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to open APT cache</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error: Not a valid Debian package</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error: Invalid or corrupted package</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error: Unmet dependencies</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error: Package conflicts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Starting installation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error:</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>HomePage</name>
     <message>
-        <location filename="../qml/HomePage.qml" line="59"/>
         <source>You can drag the deb package here</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/HomePage.qml" line="65"/>
         <source>Select file</source>
         <translation type="unfinished"></translation>
     </message>
@@ -133,7 +105,6 @@
 <context>
     <name>InstallPage</name>
     <message>
-        <location filename="../qml/InstallPage.qml" line="81"/>
         <source>Quit</source>
         <translation type="unfinished"></translation>
     </message>
@@ -141,7 +112,6 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="../qml/main.qml" line="35"/>
         <source>Package Installer</source>
         <translation type="unfinished"></translation>
     </message>
--- translations/fr_FR.ts
+++ translations/fr_FR.ts
@@ -4,52 +4,42 @@
 <context>
     <name>AppPage</name>
     <message>
-        <location filename="../qml/AppPage.qml" line="42"/>
         <source>Maintainer</source>
         <translation>Mainteneur</translation>
     </message>
     <message>
-        <location filename="../qml/AppPage.qml" line="54"/>
         <source>Homepage</source>
-        <translation>Page d'accueil</translation>
+        <translation>Page d&apos;accueil</translation>
     </message>
     <message>
-        <location filename="../qml/AppPage.qml" line="74"/>
         <source>Installed Size</source>
         <translation>Taille installée</translation>
     </message>
     <message>
-        <location filename="../qml/AppPage.qml" line="84"/>
         <source>Description</source>
         <translation>Description</translation>
     </message>
     <message>
-        <location filename="../qml/AppPage.qml" line="127"/>
         <source>Version: </source>
         <translation>Version : </translation>
     </message>
     <message>
-        <location filename="../qml/AppPage.qml" line="128"/>
         <source>Installed</source>
         <translation>Installé</translation>
     </message>
     <message>
-        <location filename="../qml/AppPage.qml" line="129"/>
         <source> (Installed Version: %1) </source>
-        <translation>(Version installée : %1)</translation>
+        <translation>(Version installée&#xa0;:&#xa0;%1)</translation>
     </message>
     <message>
-        <location filename="../qml/AppPage.qml" line="173"/>
         <source>Reinstall</source>
         <translation>Réinstaller</translation>
     </message>
     <message>
-        <location filename="../qml/AppPage.qml" line="168"/>
         <source>Cancel</source>
         <translation>Annuler</translation>
     </message>
     <message>
-        <location filename="../qml/AppPage.qml" line="173"/>
         <source>Install</source>
         <translation>Installer</translation>
     </message>
@@ -57,75 +47,57 @@
 <context>
     <name>DebInstaller</name>
     <message>
-        <location filename="../src/debinstaller.cpp" line="257"/>
-        <source>Error: Wrong architecture %1</source>
-        <translation>Erreur : Architecture incorrecte %1</translation>
-    </message>
-    <message>
-        <location filename="../src/debinstaller.cpp" line="274"/>
-        <source>Error: Breaks the existing package %1</source>
-        <translation>Erreur : Interrompt le paquet existant %1</translation>
-    </message>
-    <message>
-        <location filename="../src/debinstaller.cpp" line="282"/>
         <source>Error: Cannot satisfy dependencies</source>
-        <translation>Erreur : Ne peut pas satisfaire les dépendances</translation>
+        <translation>Erreur&#xa0;: Ne peut pas satisfaire les dépendances</translation>
     </message>
     <message>
-        <location filename="../src/debinstaller.cpp" line="290"/>
-        <source>Requires the installation of %1 additional package.</source>
-        <translation>Nécessite l'installation de %1 paquet supplémentaire.</translation>
-    </message>
-    <message>
-        <location filename="../src/debinstaller.cpp" line="292"/>
-        <source>All dependencies are satisfied.</source>
-        <translation>Toutes les dépendances sont satisfaites.</translation>
-    </message>
-    <message>
-        <location filename="../src/debinstaller.cpp" line="480"/>
-        <source>Starting</source>
-        <translation>En marche</translation>
-    </message>
-    <message>
-        <location filename="../src/debinstaller.cpp" line="486"/>
-        <source>Waiting</source>
-        <translation>En attente</translation>
-    </message>
-    <message>
-        <location filename="../src/debinstaller.cpp" line="492"/>
-        <source>Installing</source>
-        <translation>Installation</translation>
-    </message>
-    <message>
-        <location filename="../src/debinstaller.cpp" line="498"/>
-        <source>Loading Cache</source>
-        <translation>Chargement du cache</translation>
-    </message>
-    <message>
-        <location filename="../src/debinstaller.cpp" line="503"/>
-        <source>Downloading Packages</source>
-        <translation>Téléchargement des paquets</translation>
-    </message>
-    <message>
-        <location filename="../src/debinstaller.cpp" line="516"/>
         <source>Installation successful</source>
         <translation>Installation réussie</translation>
     </message>
     <message>
-        <location filename="../src/debinstaller.cpp" line="531"/>
         <source>Installation failed</source>
-        <translation>L'installation a échoué</translation>
+        <translation>L&apos;installation a échoué</translation>
+    </message>
+    <message>
+        <source>Failed to initialize APT configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to open APT cache</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error: Not a valid Debian package</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error: Invalid or corrupted package</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error: Unmet dependencies</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error: Package conflicts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Starting installation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error:</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>HomePage</name>
     <message>
-        <location filename="../qml/HomePage.qml" line="59"/>
         <source>You can drag the deb package here</source>
         <translation>Vous pouvez faire glisser le paquet deb ici</translation>
     </message>
     <message>
-        <location filename="../qml/HomePage.qml" line="65"/>
         <source>Select file</source>
         <translation>Sélectionner le dossier</translation>
     </message>
@@ -133,7 +105,6 @@
 <context>
     <name>InstallPage</name>
     <message>
-        <location filename="../qml/InstallPage.qml" line="81"/>
         <source>Quit</source>
         <translation>Quitter</translation>
     </message>
@@ -141,7 +112,6 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="../qml/main.qml" line="35"/>
         <source>Package Installer</source>
         <translation>Installateur de paquets</translation>
     </message>
